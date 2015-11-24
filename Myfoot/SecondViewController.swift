cf00023d59cf8dc6488399143cbd7681056c2d29
LucasAlumni <luca.95@live.fr>
2015-11-24 11:51:02 +0100

    Initial Commit

---
+++ Myfoot/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Myfoot
+//
+//  Created by Lucas Blanqui on 24/11/2015.
+//  Copyright © 2015 Lucas Blanqui. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
